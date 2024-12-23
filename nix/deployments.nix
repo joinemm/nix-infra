bc6ec5c6939b23810cd1a4176d2fa15ea2bd01da
Joonas Rautiola <joonas@rautiola.co>
2024-12-23 20:41:46 +0200

    Use tailscale IPs to deploy to homelab

--- nix/deployments.nix
+++ nix/deployments.nix
@@ -18,7 +18,7 @@ let
       };
     };
     nickel = {
-      hostname = "192.168.1.4";
+      hostname = "100.64.0.7";
       profiles.system = {
         user = "root";
         path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.nickel;
@@ -28,7 +28,7 @@ let
 
   aarch64 = {
     zinc = {
-      hostname = "192.168.1.3";
+      hostname = "100.64.0.3";
       profiles.system = {
         user = "root";
         path = deploy-rs.lib.aarch64-linux.activate.nixos self.nixosConfigurations.zinc;
