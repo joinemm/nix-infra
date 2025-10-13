eddd7c2e42eec5017a0912ed9fe45bf467e5ff99
Joonas Rautiola <joonas@rautiola.co>
2025-10-13 15:58:52 +0300

    Add pixel ssh key for mobile access

--- hosts/default.nix
+++ hosts/default.nix
@@ -11,8 +11,9 @@ let
     email = "joonas@rautiola.co";
     gpgKey = "0x090EB48A4669AA54";
     sshKeys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGlFqSQFoSSuAS1IjmWBFXie329I5Aqf71QhVOnLTBG+ joonas@athens"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB3h/Aj66ndKFtqpQ8H53tE9KbbO0obThC0qbQQKFQRr joonas@rome"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGlFqSQFoSSuAS1IjmWBFXie329I5Aqf71QhVOnLTBG+ joonas@athens" # Laptop
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB3h/Aj66ndKFtqpQ8H53tE9KbbO0obThC0qbQQKFQRr joonas@rome" # Desktop
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC0ONtbWZD6fMDQNWSiKLBUlkxJAVQ36jf3LbVEbba4M u0_a224@localhost" # Pixel 8
     ];
     home = "/home/${user.name}";
   };
