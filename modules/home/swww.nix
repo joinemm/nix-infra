85a05d7a7dc289c65f5fe68401192c834bead41e
Joonas Rautiola <joonas@rautiola.co>
2025-11-13 18:45:39 +0200

    Fix swww service
    Signed-off-by: Joonas Rautiola <joonas@rautiola.co>

--- modules/home/swww.nix
+++ modules/home/swww.nix
@@ -1,14 +1,20 @@
 {
   pkgs,
+  lib,
   ...
 }:
 {
   services.swww.enable = true;
 
+  # https://github.com/nix-community/home-manager/pull/8160
+  systemd.user.services.swww.Service.Environment = [
+    "PATH=$PATH:${lib.makeBinPath [ pkgs.swww ]}"
+  ];
+
   home.packages = [
     (pkgs.writeShellScriptBin "setbg" ''
       set -euo pipefail
-      swww img "$1"
+      swww img --transition-type grow --transition-duration 1 --transition-fps 60 "$1"
     '')
   ];
 }
