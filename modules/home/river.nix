b07ff703c2a2cbbe25469c6d05bb700d0ed6fc74
Joonas Rautiola <joonas@rautiola.co>
2025-11-24 19:29:14 +0200

    Refactor river config
    Signed-off-by: Joonas Rautiola <joonas@rautiola.co>

--- modules/home/river.nix
+++ modules/home/river.nix
@@ -1,10 +1,87 @@
-{ lib, pkgs, ... }:
+{
+  lib,
+  pkgs,
+  user,
+  ...
+}:
+let
+  # like flameshot but faster
+  screenshot = pkgs.writeShellApplication {
+    name = "screenshot";
+    runtimeInputs = with pkgs; [
+      grim
+      slurp
+      swappy
+      wl-clipboard
+    ];
+    text = ''
+      # no negative numbers please
+      clamp () {
+        echo "$(( $1 < 0 ? 0 : $1 ))"
+      }
+
+      # remove the riverctl rule when we're done
+      cleanup() {
+        riverctl rule-del -app-id swappy position
+      }
+      trap cleanup EXIT
+
+      # select geometry
+      G="$(slurp -o)"
+      IFS=' ,' read -r X Y _ <<<"$G"
+
+      # add riverctl rule for window positioning if the position is not zero
+      # (ie. slurp was used to select a region and not the whole screen)
+      if [ "$X" != 0 ] || [ "$Y" != 0 ]; then
+        riverctl rule-add -app-id swappy position "$(clamp $((X - 10)))" "$(clamp $((Y - 58)))"
+      fi
+
+      # take the screenshot and pass it to swappy for editing
+      grim -g "$G" - | swappy -f -
+    '';
+  };
+  screencast = pkgs.writeShellApplication {
+    name = "screencast";
+    runtimeInputs = with pkgs; [
+      procps
+      wf-recorder
+      libnotify
+      coreutils
+      slurp
+    ];
+    text = ''
+      if pgrep -x "wf-recorder"; then
+        notify-send "Stopping recording"
+        kill -2 $(pgrep wf-recorder)
+      else
+        G="$(slurp -o)"
+        notify-send "Starting to record"
+        wf-recorder -y -g "$G" -f "${user.home}/videos/screencast/$(date '+%Y-%m-%d-%H-%M-%S').mp4"
+      fi
+    '';
+  };
+in
 {
   wayland.systemd.target = "river-session.target";
+
+  xdg.configFile."swappy/config".text = ''
+    [Default]
+    save_dir=${user.home}/pictures/screenshots
+    save_filename_format=%Y%m%d-%H%M%S.png
+    line_size=3
+    text_size=18
+    text_font=monospace
+    paint_mode=brush
+    early_exit=true
+  '';
 
   home.packages = with pkgs; [
     pulseaudio
     way-displays
+    screenshot
+    screencast
+    wlogout
+    river-bsp-layout
   ];
 
   # https://codeberg.org/river/river/issues/1023#issuecomment-2272214
@@ -33,22 +110,25 @@
         listToAttrSet = list: listToAttrs (imap (i: nameValuePair (toString i)) list);
         tagMap = foldl' (x: _: x ++ [ (last x * 2) ]) [ 1 ] (genList (_: [ 1 ]) (numTags - 1));
         tagMapStrSet = listToAttrSet (map toString tagMap);
+        run = cmd: "spawn \"${cmd}\"";
+        spawn-once = cmd: args: "'pgrep -f ${cmd} && ${cmd} ${args}'";
       in
       {
-        default-layout = "bsp-layout";
         spawn = [
-          "'${lib.getExe pkgs.river-bsp-layout} --inner-gap 10 --outer-gap 10 --split-perc 0.5'"
-          "${lib.getExe pkgs.way-displays}"
-          "${lib.getExe pkgs.sway-audio-idle-inhibit}"
-          "'${lib.getExe pkgs.foot} --server'"
+          (spawn-once "river-bsp-layout" "--inner-gap 3 --outer-gap 6 --split-perc 0.5")
+          (spawn-once "way-displays" "")
+          (spawn-once "foot" "--server")
         ];
-        focus-follows-cursor = "always";
 
+        default-layout = "bsp-layout";
+        focus-follows-cursor = "normal";
         border-width = 3;
         border-color-focused = "0xFAB387";
-
+        border-color-unfocused = "0x000000";
+        border-color-urgent = "0xFF5555";
         keyboard-layout = "-options caps:super eu";
         set-repeat = "30 250";
+        allow-tearing = "enabled";
 
         input = {
           "pointer-1267-12793-ELAN067C:00_04F3:31F9_Touchpad" = {
@@ -62,82 +142,89 @@
             accel-profile = "flat";
           };
 
-          # mouse when using wireless
-          "pointer-13652-62733-Compx_LAMZU_Pro_1K_Receiver" = {
+          # Lamzu Thorn
+          "pointer-13652-*" = {
             accel-profile = "flat";
             scroll-factor = "1.5";
           };
-          # same mouse but plugged in
-          "pointer-13652-62735-compx_LAMZU_THORN" = {
+
+          # Keychron M5
+          "pointer-13364-*" = {
             accel-profile = "flat";
-            scroll-factor = "1.5";
-          };
-        };
-
-        map = {
-          normal =
-            (concatMapAttrs (index: tag: {
-              "Super ${index}" = "set-focused-tags ${tag}";
-              "Super+Shift ${index}" = "set-view-tags ${tag}";
-              "Super+Control ${index}" = "toggle-focused-tags ${tag}";
-            }) tagMapStrSet)
-            // {
-              # program hotkeys
-              "Super Return" = "spawn footclient";
-              "Super W" = "spawn zen-beta";
-              "Super R" = "spawn thunar";
-              "Super C" = "spawn 'hyprpicker -a'";
-              "Super P" = "spawn 'systemctl lock-session'";
-              "Super Space" = "spawn 'tofi-drun --drun-launch=true'";
-              "Super+Shift S" = "spawn 'flameshot-copy-fix gui'";
-              "Print" = "spawn 'flameshot-copy-fix full -p /tmp/screenshot.png'";
-              "Super Escape" = "spawn wlogout";
-              "Super Backspace" = "spawn wlogout";
-
-              # window control
-              "Super Q" = "close";
-              "Super F" = "toggle-fullscreen";
-              "Super V" = "toggle-float";
-              "Super H" = "focus-view left";
-              "Super J" = "focus-view down";
-              "Super K" = "focus-view up";
-              "Super L" = "focus-view right";
-              "Super+Shift H" = "swap left";
-              "Super+Shift J" = "swap down";
-              "Super+Shift K" = "swap up";
-              "Super+Shift L" = "swap right";
-
-              # media control
-              "None XF86AudioMute" = "spawn 'pactl set-sink-mute @DEFAULT_SINK@ toggle'";
-              "None XF86AudioMicMute" = "spawn 'pactl set-source-mute @DEFAULT_SINK@ toggle'";
-              "None XF86AudioPrev" = "spawn 'playerctl previous'";
-              "None XF86AudioNext" = "spawn 'playerctl next'";
-              "None XF86AudioPlay" = "spawn 'playerctl play-pause'";
-              "None XF86AudioMedia" = "spawn 'playerctl play-pause'";
-              "Super Comma" = "spawn 'playerctl previous'";
-              "Super Period" = "spawn 'playerctl next'";
-              "Super Slash" = "spawn 'playerctl play-pause'";
-
-              # other F keys
-              "None XF86Display" = ''spawn "wlopm --toggle '*'"'';
-            };
-
-          "-repeat normal" = {
-            "Super BracketLeft" = "send-layout-cmd bsp-layout '--dec-vsplit .01'";
-            "Super BracketRight" = "send-layout-cmd bsp-layout '--inc-vsplit .01'";
-            "Super+Shift BracketLeft" = "send-layout-cmd bsp-layout '--dec-hsplit .01'";
-            "Super+Shift BracketRight" = "send-layout-cmd bsp-layout '--inc-hsplit .01'";
-
-            "None XF86AudioRaiseVolume" = "spawn 'pactl set-sink-volume @DEFAULT_SINK@ +5%'";
-            "None XF86AudioLowerVolume" = "spawn 'pactl set-sink-volume @DEFAULT_SINK@ -5%'";
-            "None XF86MonBrightnessUp" = "spawn 'brightnessctl s +5%'";
-            "None XF86MonBrightnessDown" = "spawn 'brightnessctl s 5%-'";
+            scroll-factor = "1";
           };
         };
 
         rule-add = {
           # server side decorations for everything
           "ssd" = "";
+        };
+
+        map-pointer.normal = {
+          "Super BTN_LEFT" = "move-view";
+          "Super BTN_RIGHT" = "resize-view";
+          "Super BTN_MIDDLE" = "toggle-float";
+        };
+
+        map.normal =
+          (concatMapAttrs (index: tag: {
+            "Super ${index}" = "set-focused-tags ${tag}";
+            "Super+Shift ${index}" = "set-view-tags ${tag}";
+            "Super+Control ${index}" = "toggle-focused-tags ${tag}";
+          }) tagMapStrSet)
+          // {
+            # program hotkeys
+            "Super Return" = run "footclient --no-wait";
+            "Super W" = run "glide";
+            "Super R" = run "thunar";
+            "Super C" = run "hyprpicker -a";
+            "Super L" = run "systemctl lock-session";
+            "Super Space" = run "tofi-drun --drun-launch=true";
+            "Super+Shift S" = run "screenshot";
+            "Super+Shift+Alt S" = run "screencast";
+            "None Print" = run "screenshot";
+            "Shift Print" = run "screencast";
+            "Super Escape" = run "wlogout";
+            "Super Backspace" = run "wlogout";
+
+            # window control
+            "Super Q" = "close";
+            "Super F" = "toggle-fullscreen";
+            "Super V" = "toggle-float";
+            "Super Tab" = "focus-view next";
+            "Super+Shift Tab" = "focus-view previous";
+            "Super+Alt Tab" = "swap next";
+            "Super+Shift+Alt Tab" = "swap previous";
+            "Super+Shift Page_up" = "send-to-output next";
+            "Super+Shift Page_down" = "send-to-output previous";
+
+            # media control
+            "None XF86AudioMute" = run "pactl set-sink-mute @DEFAULT_SINK@ toggle";
+            "None XF86AudioMicMute" = run "pactl set-source-mute @DEFAULT_SINK@ toggle";
+            "None XF86AudioPrev" = run "playerctl previous";
+            "None XF86AudioNext" = run "playerctl next";
+            "None XF86AudioPlay" = run "playerctl play-pause";
+            "None XF86AudioMedia" = run "playerctl play-pause";
+            "Super Comma" = run "playerctl previous";
+            "Super Period" = run "playerctl next";
+            "Super Slash" = run "playerctl play-pause";
+
+            # other F keys
+            "None XF86Display" = run "wlopm --toggle '*'";
+
+            "None F8" = run "pkill -SIGUSR1 -f gpu-screen-recorder";
+          };
+
+        map."-repeat normal" = {
+          "Super BracketLeft" = "send-layout-cmd bsp-layout '--dec-vsplit .01'";
+          "Super BracketRight" = "send-layout-cmd bsp-layout '--inc-vsplit .01'";
+          "Super+Shift BracketLeft" = "send-layout-cmd bsp-layout '--dec-hsplit .01'";
+          "Super+Shift BracketRight" = "send-layout-cmd bsp-layout '--inc-hsplit .01'";
+
+          "None XF86AudioRaiseVolume" = run "pactl set-sink-volume @DEFAULT_SINK@ +5%";
+          "None XF86AudioLowerVolume" = run "pactl set-sink-volume @DEFAULT_SINK@ -5%";
+          "None XF86MonBrightnessUp" = run "brightnessctl s +5%";
+          "None XF86MonBrightnessDown" = run "brightnessctl s 5%-";
         };
       };
   };
