45d42562760340a16725e4b1205500c0924c317b
Joonas Rautiola <joonas@rautiola.co>
2025-12-02 12:29:45 +0200

    Make thunderbird default for opening email links
    Signed-off-by: Joonas Rautiola <joonas@rautiola.co>

--- modules/home/xdg.nix
+++ modules/home/xdg.nix
@@ -42,18 +42,17 @@
             image-viewer = "swayimg.desktop";
           in
           {
-            "application/pdf" = [ "sioyek.desktop" ];
+            # images
             "image/gif" = [ image-viewer ];
             "image/jpeg" = [ image-viewer ];
             "image/png" = [ image-viewer ];
             "image/webp" = [ image-viewer ];
-            "inode/directory" = [ file-manager ];
-            "text/csv" = [ editor ];
-            "text/html" = [ browser ];
-            "text/plain" = [ editor ];
+            # videos
             "video/mp4" = [ video-player ];
             "video/webm" = [ video-player ];
             "video/x-matroska" = [ video-player ];
+            # browser
+            "text/html" = [ browser ];
             "x-scheme-handler/http" = [ browser ];
             "x-scheme-handler/https" = [ browser ];
             "x-scheme-handler/chrome" = [ browser ];
@@ -63,6 +62,25 @@
             "application/xhtml+xml" = [ browser ];
             "application/x-extension-xhtml" = [ browser ];
             "application/x-extension-xht" = [ browser ];
+            # thunderbird
+            "x-scheme-handler/mailto" = [ "userapp-Thunderbird-FSRFG3.desktop" ];
+            "message/rfc822" = [ "userapp-Thunderbird-FSRFG3.desktop" ];
+            "x-scheme-handler/mid" = [ "userapp-Thunderbird-FSRFG3.desktop" ];
+            "x-scheme-handler/news" = [ "userapp-Thunderbird-LB3HG3.desktop" ];
+            "x-scheme-handler/snews" = [ "userapp-Thunderbird-LB3HG3.desktop" ];
+            "x-scheme-handler/nntp" = [ "userapp-Thunderbird-LB3HG3.desktop" ];
+            "x-scheme-handler/feed" = [ "userapp-Thunderbird-YG0GG3.desktop" ];
+            "application/rss+xml" = [ "userapp-Thunderbird-YG0GG3.desktop" ];
+            "application/x-extension-rss" = [ "userapp-Thunderbird-YG0GG3.desktop" ];
+            "x-scheme-handler/webcal" = [ "userapp-Thunderbird-04IDG3.desktop" ];
+            "text/calendar" = [ "userapp-Thunderbird-04IDG3.desktop" ];
+            "application/x-extension-ics" = [ "userapp-Thunderbird-04IDG3.desktop" ];
+            "x-scheme-handler/webcals" = [ "userapp-Thunderbird-04IDG3.desktop" ];
+            # other
+            "inode/directory" = [ file-manager ];
+            "text/csv" = [ editor ];
+            "text/plain" = [ editor ];
+            "application/pdf" = [ "sioyek.desktop" ];
             "x-scheme-handler/magnet" = [ "transmission-magnet.desktop" ];
             "x-scheme-handler/prusaslicer" = [ "PrusaSlicerURLProtocol.desktop" ];
             "application/vnd.openxmlformats-officedocument.wordprocessingml.document" = [
