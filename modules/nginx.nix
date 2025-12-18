3e8633783e03ddb2c6a58ab36ac684f06ee90a2a
Joonas Rautiola <joonas@rautiola.co>
2025-12-18 13:34:29 +0200

    Add a default nginx server block to catch non-matching requests
    Signed-off-by: Joonas Rautiola <joonas@rautiola.co>

--- modules/nginx.nix
+++ modules/nginx.nix
@@ -1,4 +1,10 @@
-{ config, user, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  user,
+  ...
+}:
 {
   security.acme = {
     acceptTerms = true;
@@ -22,6 +28,42 @@
       proxy_headers_hash_max_size 512;
       proxy_headers_hash_bucket_size 128;
     '';
+
+    # default server block when nothing else matches.
+    # 444 will drop the connection
+    virtualHosts."_" = {
+      default = true;
+      addSSL = true;
+      sslCertificate = "/etc/nginx/dummy_ssl/default.crt";
+      sslCertificateKey = "/etc/nginx/dummy_ssl/default.key";
+      extraConfig = ''
+        return 444;
+      '';
+    };
+  };
+
+  systemd.services.create-dummy-cert = {
+    wantedBy = [ "nginx.service" ];
+    before = [ "nginx.service" ];
+
+    serviceConfig = {
+      Type = "oneshot";
+      Restart = "no";
+    };
+
+    script = # sh
+      ''
+        DIR="/etc/nginx/dummy_ssl"
+        if [[ ! -d "$DIR" ]]; then
+          mkdir -p "$DIR"
+          ${lib.getExe pkgs.openssl} req -x509 -nodes -newkey rsa:2048 \
+            -keyout "$DIR/default.key" \
+            -out "$DIR/default.crt" \
+            -days 3650 \
+            -subj "/CN=invalid"
+          chown -R nginx:nginx "$DIR"
+        fi
+      '';
   };
 
   networking.firewall.allowedTCPPorts = [
