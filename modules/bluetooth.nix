bebfb4a54e550a318c65c1938a9192a8e0ea3c5e
Joonas Rautiola <joonas@rautiola.co>
2026-04-20 21:09:07 +0300

    Fix bluetooth headset

--- modules/bluetooth.nix
+++ modules/bluetooth.nix
@@ -7,13 +7,20 @@
       package = pkgs.bluez5-experimental;
       settings.General = {
         Experimental = true;
-        FastConnectable = true;
       };
     };
   };
 
-  # remember the bluetooth device profile when reconnecting
-  services.pulseaudio.extraConfig = ''
-    load-module module-card-restore restore_bluetooth_profile=true
-  '';
+  # default pulseaudio config loads these modules already.
+  # they must be unloaded first if we want to add parameters to them
+  services.pulseaudio.extraConfig = # sh
+    ''
+      # switch bluetooth profile automatically to HSP/headset when mic is requested
+      unload-module module-bluetooth-policy
+      load-module module-bluetooth-policy auto_switch=2
+
+      # remember the bluetooth device profile when reconnecting
+      unload-module module-card-restore
+      load-module module-card-restore restore_bluetooth_profile=true
+    '';
 }
