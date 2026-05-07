7235e4783e6c95638ebe7e4ea2cf20d27cc98891
Joonas Rautiola <joonas@rautiola.co>
2026-05-07 12:54:33 +0300

    CI fix #5

--- nix/githubMatrix.nix
+++ nix/githubMatrix.nix
@@ -3,4 +3,9 @@
   flake.githubActions.matrix = {
     host = builtins.attrNames self.nixosConfigurations;
   };
+
+  flake.ci-targets = {
+    packages = self.packages.x86_64-linux;
+    hosts = builtins.mapAttrs (_name: cfg: cfg.config.system.build.toplevel) self.nixosConfigurations;
+  };
 }
