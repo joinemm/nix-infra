4af27fb8c631a961f19ad432f14fe3de5c09aa98
Joonas Rautiola <joonas@rautiola.co>
2026-07-09 17:11:26 +0300

    Set up nix-ld to run precompiled binaries

--- modules/dev.nix
+++ modules/dev.nix
@@ -23,4 +23,28 @@
 
   # visit astro dev instance from another device such as phone
   networking.firewall.allowedTCPPorts = [ 4321 ];
+
+  programs.nix-ld = {
+    enable = true;
+    libraries = with pkgs; [
+      stdenv.cc.cc # C compiler runtime
+      openssl # TLS/SSL library
+      curl # HTTP client library
+      glib # Core library
+      util-linux # Linux utilities
+      glibc # C standard library
+      icu # Unicode support
+      libunwind # Stack unwinding
+      libuuid # UUID generation
+      zlib # Compression library
+      libsecret # Secure credential storage
+      freetype # Font rendering
+      libglvnd # OpenGL vendor library
+      libnotify # Desktop notifications
+      SDL2 # Graphics library
+      vulkan-loader # Vulkan API loader
+      gdk-pixbuf # Image loading library
+      fuse # Filesystem in userspace
+    ];
+  };
 }
