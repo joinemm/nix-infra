61651215ee6cf502d3d61510325537b457575aa5
Joonas Rautiola <joonas@rautiola.co>
2026-09-15 12:09:51 +0300

    Migrate immich configuration to declarative config file

--- hosts/nickel/immich.nix
+++ hosts/nickel/immich.nix
@@ -1,11 +1,36 @@
 { config, mkBackup, ... }:
 {
+  sops.secrets.immich_oidc_client_secret = {
+    owner = config.services.immich.user;
+    restartUnits = [ "immich-server.service" ];
+  };
+
   services.immich = {
     enable = true;
-    host = "0.0.0.0";
+    host = "127.0.0.1";
     environment = {
       TZ = "Europe/Helsinki";
       IMMICH_TELEMETRY_INCLUDE = "all"; # expose all prometheus metrics
+    };
+    settings = {
+      oauth = {
+        enabled = true;
+        clientId = "immich";
+        clientSecret._secret = config.sops.secrets.immich_oidc_client_secret.path;
+        issuerUrl = "https://auth.lab.joinemm.dev";
+        autoLaunch = true;
+        buttonText = "Login with Authelia";
+        tokenEndpointAuthMethod = "client_secret_basic";
+      };
+      server = {
+        externalDomain = "https://photos.joinemm.dev";
+      };
+      passwordLogin.enabled = false;
+      newVersionCheck.enabled = false;
+      storageTemplate = {
+        enabled = true;
+        template = "{{y}}/{{MM}}-{{MMMM}}/{{filename}}";
+      };
     };
   };
 
